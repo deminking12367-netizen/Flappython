e3ea024eafbff996fa4092a18ea20f1381e230ab
deminking12367-netizen <deminking12367@gmail.com>
2025-09-12 04:52:05 -0700

    renamed:    code/Imports.py -> Imports.py 	new file:   Music.py 	new file:   Objects.py 	renamed:    code/Variables.py -> Variables.py 	renamed:    code/__pycache__/Imports.cpython-313.pyc -> __pycache__/Imports.cpython-313.pyc 	renamed:    code/__pycache__/Music.cpython-313.pyc -> __pycache__/Music.cpython-313.pyc 	renamed:    code/__pycache__/Objects.cpython-313.pyc -> __pycache__/Objects.cpython-313.pyc 	renamed:    code/__pycache__/Variables.cpython-313.pyc -> __pycache__/Variables.cpython-313.pyc 	renamed:    code/__pycache__/settings.cpython-313.pyc -> __pycache__/settings.cpython-313.pyc 	renamed:    code/audio/DEATH.mp3 -> audio/DEATH.mp3 	renamed:    code/audio/Point.mp3 -> audio/Point.mp3 	renamed:    code/audio/cartoon-jump-6462.mp3 -> audio/cartoon-jump-6462.mp3 	renamed:    code/audio/friess.MP3 -> audio/friess.MP3 	deleted:    code/Music.py 	deleted:    code/Objects.py 	renamed:    code/graphics/Ground.png -> graphics/Ground.png 	renamed:    code/graphics/Menu_screen.png -> graphics/Menu_screen.png 	renamed:    code/graphics/Pixeltype.ttf -> graphics/Pixeltype.ttf 	renamed:    code/graphics/Sky.png -> graphics/Sky.png 	renamed:    code/graphics/cloud.png -> graphics/cloud.png 	renamed:    code/graphics/durpthon/durpthon.png -> graphics/durpthon/durpthon.png 	renamed:    code/graphics/durpthon/durpthon_jump.png -> graphics/durpthon/durpthon_jump.png 	renamed:    code/graphics/pipe/pipedown.png -> graphics/pipe/pipedown.png 	renamed:    code/graphics/pipe/pipeup.png -> graphics/pipe/pipeup.png 	renamed:    code/main.py -> main.py 	renamed:    code/settings.py -> settings.py

--- code/Music.py
+++
@@ -1,9 +0,0 @@
-from settings import *
-
-# Music
-
-Bg_Music = pygame.mixer.Sound(join('code','audio','friess.MP3'))
-jump = pygame.mixer.Sound(join('code','audio','cartoon-jump-6462.mp3'))
-point = pygame.mixer.Sound(join('code','audio','Point.mp3'))
-Bg_Music.play(loops=-1)
-death =  pygame.mixer.Sound(join('code','audio','DEATH.mp3'))
