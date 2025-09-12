8dc99ee51c6d698c0a87fe022971c229c2869a05
deminking12367-netizen <deminking12367@gmail.com>
2025-09-11 21:06:06 -0700

    starting

--- code/Music.py
+++ code/Music.py
@@ -2,8 +2,8 @@ from settings import *
 
 # Music
 
-Bg_Music = pygame.mixer.Sound(join('audio','friess.MP3'))
-jump = pygame.mixer.Sound(join('audio','cartoon-jump-6462.mp3'))
-point = pygame.mixer.Sound(join('audio','Point.mp3'))
+Bg_Music = pygame.mixer.Sound(join('code','audio','friess.MP3'))
+jump = pygame.mixer.Sound(join('code','audio','cartoon-jump-6462.mp3'))
+point = pygame.mixer.Sound(join('code','audio','Point.mp3'))
 Bg_Music.play(loops=-1)
-death =  pygame.mixer.Sound(join('audio','DEATH.mp3'))
+death =  pygame.mixer.Sound(join('code','audio','DEATH.mp3'))
